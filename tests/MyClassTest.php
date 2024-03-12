477a01f5eef19a2ad4dd4fde40476ae7a8edb4e0
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2024-03-12 12:20:10 -0400

    #1 Setup phpunit.
    - Update local VSC tasks for testing.
    - Add test script for PHPUnit.
    - Update gitignore for PHPUnit.
    - Gratis dummy test case.

---
+++ tests/MyClassTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AlexanderAllen\Panettone;
+
+use PHPUnit\Framework\TestCase;
+
+class MyClassTest extends TestCase
+{
+    public function testFooBar(): void
+    {
+        $this->assertEquals(true, true);
+    }
+}
