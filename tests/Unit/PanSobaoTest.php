2d7c5f8eb46bd755554910f43d5c8a00625aee06
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2024-03-15 19:34:02 -0400

    BUG: document broken use case with prop generator.
    TL;DR: It's not a cebe issue, its' api-platform.

--- tests/Unit/PanSobaoTest.php
+++ tests/Unit/PanSobaoTest.php
@@ -7,7 +7,7 @@ namespace AlexanderAllen\Panettone\Test\Unit;
 // use AlexanderAllen\Panettone\ClassGenerator;
 use AlexanderAllen\Panettone\Bread\PanSobao;
 use cebe\openapi\{Reader, ReferenceContext};
-use cebe\openapi\spec\{OpenApi, Schema, Reference};
+use cebe\openapi\spec\{OpenApi, Schema, Reference, Components};
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\{CoversClass, Group, Test, TestDox, Large};
 use Symfony\Component\Console\Output\ConsoleOutput;
@@ -16,6 +16,9 @@ use ApiPlatform\SchemaGenerator\OpenApi\Model\Class_;
 use ApiPlatform\SchemaGenerator\OpenApi\PropertyGenerator\PropertyGenerator;
 use ApiPlatform\SchemaGenerator\PropertyGenerator\PropertyGeneratorInterface;
 use ApiPlatform\SchemaGenerator\OpenApi\Model\Property;
+use cebe\openapi\exceptions\TypeErrorException;
+use cebe\openapi\exceptions\UnresolvableReferenceException;
+use cebe\openapi\exceptions\IOException;
 use Psr\Log\LoggerAwareTrait;
 
 /**
@@ -32,11 +35,57 @@ class PanSobaoTest extends TestCase
     use LoggerAwareTrait;
 
     private PropertyGeneratorInterface $propertyGenerator;
+    private string $fixtureSchemaError;
 
+    /**
+     * Simple references will always give
+     * [error] Error sourcing schema: "ApiPlatform\SchemaGenerator\OpenApi\Model\Type\PrimitiveType::__construct(): Argument #1 ($name) must be of type string, null given, called in ...src/OpenApi/PropertyGenerator/PropertyGenerator.php on line 116"
+     * @return void
+     */
     public function setUp(): void
     {
         $output = new ConsoleOutput(ConsoleOutput::VERBOSITY_DEBUG);
         self::setLogger(new ConsoleLogger($output));
+
+        /**
+         * Simple reference test / example from SmartBear.
+         * See "Nested Objects" in https://swagger.io/docs/specification/data-models/data-types/
+         *
+         * 3/15
+         * cebe doesn't have any problem reading this, it's the api-platform prop generator.
+         */
+        $this->fixtureSchemaError = <<<'schemaFixture'
+        openapi: 3.0.1
+        info:
+          title: Panettone
+          version: 1.0.0
+          description: Test schema for Panettone
+        servers:
+          - url: https://localhost
+        paths:
+          /me:
+            get:
+              summary: Returns the authenticated users information.
+        components:
+          schemas:
+            User:
+              type: object
+              properties:
+                id:
+                  type: integer
+                name:
+                  type: string
+                contact_info:
+                  $ref: '#/components/schemas/ContactInfo'
+            ContactInfo:
+              type: object
+              properties:
+                email:
+                  type: string
+                  format: email
+                phone:
+                  type: string
+        schemaFixture;
     }
 
     #[Test]
@@ -52,6 +101,42 @@ class PanSobaoTest extends TestCase
     }
 
     /**
+     * All `Reference` objects will be replaced with their referenced spec objects.
+     *
+     * @return void
+     * @throws TypeErrorException
+     */
+    #[Test]
+    #[TestDox('Component references are replaced')]
+    public function third(): void
+    {
+        $this->propertyGenerator = new PropertyGenerator();
+
+        $openapi = Reader::readFromYaml(
+            $this->fixtureSchemaError,
+            OpenApi::class,
+            true
+        );
+
+        $classes = [];
+        try {
+            foreach ($openapi->components->schemas as $name => $schema) {
+                $this->logger->info(sprintf('Source schema "%s"', $name));
+                \assert($schema instanceof Schema);
+                $classes[] = $this->buildClassFromSchema($name, $schema);
+            }
+            $test = null;
+        } catch (\cebe\openapi\exceptions\TypeErrorException $error) {
+            $this->logger->error('The YAML provided is whack');
+            throw $error;
+        } catch (\Throwable $source) {
+            $this->logger->error(sprintf('Error sourcing schema: "%s"', $source->getMessage()));
+        }
+
+        $test = null;
+    }
+
+    /**
      * Alternative loop using while instead of foreach.
      *
      * 3/14
@@ -63,8 +148,11 @@ class PanSobaoTest extends TestCase
      * ClassGenerator brought inline for better control and visibility.
      * Next step is to source reproducible, atomic schemas that can be actually
      * be unit tested, as opposed to production behemoths.
+     *
+     * For example: make sure that a compoment containing a reference has the
+     * reference replaced in the result.
      */
-    #[Test]
+    // #[Test]
     #[TestDox('test class generator')]
     public function second(): void
     {
