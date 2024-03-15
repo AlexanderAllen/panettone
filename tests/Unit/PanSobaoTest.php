92d39fd63b32ee225a4ee70416915cce09182ba9
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2024-03-15 16:56:22 -0400

    PanSobao WIP

--- tests/Unit/PanSobaoTest.php
+++ tests/Unit/PanSobaoTest.php
@@ -15,6 +15,7 @@ use Symfony\Component\Console\Logger\ConsoleLogger;
 use ApiPlatform\SchemaGenerator\OpenApi\Model\Class_;
 use ApiPlatform\SchemaGenerator\OpenApi\PropertyGenerator\PropertyGenerator;
 use ApiPlatform\SchemaGenerator\PropertyGenerator\PropertyGeneratorInterface;
+use ApiPlatform\SchemaGenerator\OpenApi\Model\Property;
 use Psr\Log\LoggerAwareTrait;
 
 /**
@@ -38,38 +39,30 @@ class PanSobaoTest extends TestCase
         self::setLogger(new ConsoleLogger($output));
     }
 
-    /**
-     * Simple loop test w/ logger insider generator.
-     */
     #[Test]
-    #[TestDox('Generator loop using foreach')]
+    #[TestDox('Dummy generator loop with foreach')]
     public function first(): void
     {
         $class = new PanSobao();
+        $yields = [];
         foreach ($class->generate() as $key => $value) {
-            // echo $key, ' => ', $value, "\n";
-            // self::assertIsInt($value);
-            self::assertNotNull($value);
+            $yields[$key] = $value;
         }
+        self::assertCount(3, $yields);
     }
 
     /**
      * Alternative loop using while instead of foreach.
      *
-     * Read top level open api schema file.
-     * Feed to class generator.
-     * Process "actual" generator?
-     *
-     * TODO some naming conventions needed here,
-     * class generator is confusing with actual `Generator`s.
-     *
-     * Generation happens in multiple phases, I'd be more conforable using a phase-like
-     * naming convention for functions.
-     *
      * 3/14
      * This test is waaay too large.
      * I need to test the inner components of the schema generators, and doing
      * so from the upper ClassGenerator is not gonna cut it.
+     *
+     * 3/15
+     * ClassGenerator brought inline for better control and visibility.
+     * Next step is to source reproducible, atomic schemas that can be actually
+     * be unit tested, as opposed to production behemoths.
      */
     #[Test]
     #[TestDox('test class generator')]
@@ -84,17 +77,6 @@ class PanSobaoTest extends TestCase
             ReferenceContext::RESOLVE_MODE_INLINE
         );
 
-        // Second phase, setup class processor.
-        // The class processor will invoke recursively the schema generator.
-        // $genclass = new ClassGenerator();
-        // $output = new ConsoleOutput(ConsoleOutput::VERBOSITY_DEBUG);
-        // $genclass->setLogger(new ConsoleLogger($output));
-
-        // Initiate sourcing process.
-        // 3/15 skip top level sourcing and do direclty in test.
-        // Keep drilling down till we get directly to the recursive bits.
-        // $genclass->sourceSchemaComponents($openapi); // moved to test
-
         $classes = [];
         try {
             foreach ($openapi->components->schemas as $name => $schema) {
@@ -103,8 +85,7 @@ class PanSobaoTest extends TestCase
                 $classes[] = $this->buildClassFromSchema($name, $schema);
             }
         } catch (\Throwable $source) {
-            // 3/15 what's the best practice for logging throwables in tests.
-            // $this->logger->error(sprintf('Error sourcing schema "%s"', $name));
+            $this->logger->error(sprintf('Error sourcing schema "%s"', $name));
         }
     }
 
@@ -118,7 +99,7 @@ class PanSobaoTest extends TestCase
      *
      * @param \Generator<int, Schema|Reference> $generator
      *
-     * @return list<Schema|Reference>
+     * @return array<string, Schema|Reference>
      *   Array containing the properties of a given Schema component.
      */
     private function iterateGenerator(\Generator $generator): array
@@ -131,7 +112,8 @@ class PanSobaoTest extends TestCase
 
             // Only Schema types have properties, Reference types do not.
             if ($schemaItem instanceof Schema) {
-                self::assertObjectHasProperty('properties', $schemaItem);
+                // Maybe dont run test assertions in inner scopes?
+                // self::assertObjectHasProperty('properties', $schemaItem);
                 $schemaProperties = array_merge($schemaProperties, $schemaItem->properties);
             }
             // Invoke the generator to move forward the internal pointer.
@@ -140,28 +122,33 @@ class PanSobaoTest extends TestCase
         return $schemaProperties;
     }
 
+    /**
+     * Builds in-memory class from given cebe schema.
+     *
+     * @param string $name
+     * @param Schema $schema
+     * @return Class_
+     */
     private function buildClassFromSchema(string $name, Schema $schema): Class_
     {
         $class = new Class_($name);
 
         $pan = new PanSobao();
-        $output = new ConsoleOutput(ConsoleOutput::VERBOSITY_DEBUG);
+        $output = new ConsoleOutput(ConsoleOutput::VERBOSITY_DEBUG); // setup logger for generator
         $pan->setLogger(new ConsoleLogger($output));
 
         // Initial generator call should not yield.
+        /**
+         * A good unit test would be confirming what happens when you feed
+         * a schema with References.
+         */
         $gen = $pan->generator($schema);
         $schemaProperties = $this->iterateGenerator($gen);
 
         foreach ($schemaProperties as $propertyName => $schemaProperty) {
-            // Will this fail on references too?
-            // Call to function assert() with true will always evaluate to true.PHPStan
-            // \assert($schemaProperty instanceof \cebe\openapi\SpecObjectInterface);
+            $this->logger->info(sprintf('Source property named "%s"', $propertyName));
 
-            // $this->logger->info(sprintf('Source property named "%s"', $propertyName));
-
-            // 3/15 linking to yet another external dependency, in this case the property generator class.
-            // I don't see any reason why this should not be internal to the class.
-            // It's all related to the same behavior, so it's not a trying to do too much use case.
+            // 3/15 prop generator is external because it is not exactly small.
             $property =
             ($this->propertyGenerator)(
                 $propertyName,
@@ -171,6 +158,7 @@ class PanSobaoTest extends TestCase
             );
 
             if ($property !== null) {
+                \assert($property instanceof Property);
                 $class->addProperty($property);
             }
         }
