9777f80b9957f8533a6199861398cabee5f8da46
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2024-03-22 00:19:53 -0400

    Hard mode activated.

--- tests/Unit/MedianocheTest.php
+++ tests/Unit/MedianocheTest.php
@@ -123,7 +123,7 @@ class MedianocheTest extends TestCase
         $logger = new ConsoleLogger(new ConsoleOutput(ConsoleOutput::VERBOSITY_DEBUG));
         self::setLogger($logger);
         $spec = Reader::readFromYamlFile(
-            realpath('tests/fixtures/medianoche.yml'),
+            realpath('tests/fixtures/medianoche-1.yml'),
             OpenAPI::class,
             ReferenceContext::RESOLVE_MODE_ALL,
         );
