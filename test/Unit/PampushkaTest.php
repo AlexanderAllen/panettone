176f9d732de6209a089b10fcdae974ebbaa38451
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2024-04-30 19:23:11 -0400

    Cleanup command test.

--- test/Unit/PampushkaTest.php
+++ test/Unit/PampushkaTest.php
@@ -6,7 +6,6 @@ namespace AlexanderAllen\Panettone\Test\Unit;
 
 use AlexanderAllen\Panettone\Test\Setup;
 use AlexanderAllen\Panettone\Setup as ParentSetup;
-use AlexanderAllen\Panettone\Bread\MediaNoche;
 use AlexanderAllen\Panettone\Bread\PanDeAgua;
 use AlexanderAllen\Panettone\Command\Main;
 use PHPUnit\Framework\Attributes\{CoversClass, Group, TestDox, UsesClass};
@@ -23,7 +22,6 @@ use Symfony\Component\Console\Tester\CommandTester;
  * @see https://github.com/api-platform/schema-generator/blob/997f6f811faa75006aeff72cec26fe291bb8eaab/tests/Command/GenerateCommandTest.php
  */
 #[UsesClass(PanDeAgua::class)]
-#[UsesClass(MediaNoche::class)]
 #[UsesClass(ParentSetup::class)]
 #[CoversClass(Main::class)]
 #[TestDox('Pampushka')]
@@ -35,6 +33,7 @@ class PampushkaTest extends TestCase
     #[TestDox('Test command')]
     public function testCommand(): void
     {
+        // Statically cache a valid settings location for the command.
         PanDeAgua::getSettings("test/schema/settings.ini");
 
         $input = ['source' => 'test/schema/keyword-anyOf-simple.yml'];
