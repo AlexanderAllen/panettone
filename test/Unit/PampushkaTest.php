9b2e5ff530469cf9d2af3bd4a7b8137e10b6db8e
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2024-04-30 19:04:24 -0400

    Cleanup command test.

--- test/Unit/PampushkaTest.php
+++ test/Unit/PampushkaTest.php
@@ -9,22 +9,9 @@ use AlexanderAllen\Panettone\Setup as ParentSetup;
 use AlexanderAllen\Panettone\Bread\MediaNoche;
 use AlexanderAllen\Panettone\Bread\PanDeAgua;
 use AlexanderAllen\Panettone\Command\Main;
-use PHPUnit\Framework\Attributes\{CoversClass, Group, Test, TestDox, UsesClass};
-use Nette\PhpGenerator\ClassType;
-use Nette\PhpGenerator\Printer;
-use Nette\PhpGenerator\Helpers;
-use Nette\PhpGenerator\Method;
-use Nette\PhpGenerator\PhpFile;
-use Nette\PhpGenerator\PhpNamespace;
-use Nette\PhpGenerator\Property;
-use PHPUnit\Framework\Exception;
-use PHPUnit\Framework\ExpectationFailedException;
-use Nette\PhpGenerator\Type;
-use Nette\Utils\Type as UtilsType;
+use PHPUnit\Framework\Attributes\{CoversClass, Group, TestDox, UsesClass};
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Tester\CommandTester;
-use Symfony\Component\Filesystem\Filesystem;
 
 /**
  * Test for command line application.
@@ -44,41 +31,11 @@ class PampushkaTest extends TestCase
 {
     use Setup;
 
-    /**
-     * Test for configuration files.
-     *
-     * Things that I need in settings:
-     * - Type output path, currently hardcoded usually to tmp.
-     * - Namespace to be used for generated types.
-     *
-     * @return void
-     */
     #[Group('target')]
     #[TestDox('Test command')]
     public function testCommand(): void
     {
-        $settings = PanDeAgua::getSettings("test/schema/settings.ini");
-        $output_path = $settings['file']['output_path'];
-        $namespace = $settings['file']['namespace'];
-        [$spec, $printer] = $this->realSetup('test/schema/keyword-anyOf-simple.yml', true);
-
-        $classes = [];
-        foreach ($spec->components->schemas as $name => $schema) {
-            $class = MediaNoche::newNetteClass($schema, $name);
-            $classes[$name] = $class;
-            // $this->logger->debug($printer->printClass($class));
-        }
-
-        foreach ($classes as $name => $class_type) {
-            PanDeAgua::printFile($printer, $class_type, $settings);
-
-            $target = sprintf('%s/%s.php', $output_path, $name);
-            $file = PhpFile::fromCode(file_get_contents($target));
-
-            // Testing for existance of single namespace.
-            $namespaces = $file->getNamespaces();
-            self::assertArrayHasKey($namespace, $namespaces, 'Generated file contains specified namespace');
-        }
+        PanDeAgua::getSettings("test/schema/settings.ini");
 
         $input = ['source' => 'test/schema/keyword-anyOf-simple.yml'];
         $commandTester = new CommandTester(new Main());
