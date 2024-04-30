e9853eac807c90baf07226a280eacd98252ae73a
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2024-04-30 19:56:30 -0400

    Assert bad source results in command failure.

--- test/Unit/PampushkaTest.php
+++ test/Unit/PampushkaTest.php
@@ -7,6 +7,7 @@ namespace AlexanderAllen\Panettone\Test\Unit;
 use AlexanderAllen\Panettone\Test\Setup;
 use AlexanderAllen\Panettone\Setup as ParentSetup;
 use AlexanderAllen\Panettone\Bread\PanDeAgua;
+use AlexanderAllen\Panettone\Bread\MediaNoche;
 use AlexanderAllen\Panettone\Command\Main;
 use PHPUnit\Framework\Attributes\{CoversClass, Group, TestDox, UsesClass};
 use PHPUnit\Framework\TestCase;
@@ -22,6 +23,7 @@ use Symfony\Component\Console\Tester\CommandTester;
  * @see https://github.com/api-platform/schema-generator/blob/997f6f811faa75006aeff72cec26fe291bb8eaab/tests/Command/GenerateCommandTest.php
  */
 #[UsesClass(PanDeAgua::class)]
+#[UsesClass(MediaNoche::class)]
 #[UsesClass(ParentSetup::class)]
 #[CoversClass(Main::class)]
 #[TestDox('Pampushka')]
@@ -39,5 +41,14 @@ class PampushkaTest extends TestCase
         $input = ['source' => 'test/schema/keyword-anyOf-simple.yml'];
         $commandTester = new CommandTester(new Main());
         $this->assertEquals(0, $commandTester->execute($input, []));
+    }
+
+    #[Group('target')]
+    #[TestDox('Assert bad source results in command failure')]
+    public function testCommandFail(): void
+    {
+        $input = ['source' => 'test/schema/bad-source.yml'];
+        $commandTester = new CommandTester(new Main());
+        $this->assertEquals(1, $commandTester->execute($input, []));
     }
 }
