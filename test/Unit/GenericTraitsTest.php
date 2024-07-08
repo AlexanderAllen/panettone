67dabf84110083451851d603156ee36c543dfb36
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2024-07-08 03:48:29 -0400

    Rename generic  traits test.

---
+++ test/Unit/GenericTraitsTest.php
@@ -0,0 +1,124 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AlexanderAllen\Panettone\Test\Unit;
+
+use FunctionalPHP\FantasyLand\Apply as ApplyInterface;
+use FunctionalPHP\FantasyLand\Chain;
+use FunctionalPHP\FantasyLand\Functor as FantasyFunctor;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\{CoversNothing, Group, Test, TestDox};
+use Widmogrod\Common\PointedTrait;
+use Widmogrod\Common\ValueOfTrait;
+
+/**
+ * How to deploy PHPStan generics when using traits.
+ *
+ * This dead simple example demonstrates that the type information is not lost
+ * when using generics through traits.
+ *
+ * @package AlexanderAllen\Panettone\Test
+ */
+#[TestDox('Using traits with PHPStan generics')]
+#[CoversNothing]
+#[Group('target')]
+class GenericTraits extends TestCase
+{
+    #[Test]
+    public function testGenericContainers(): void
+    {
+        // Generic variable is properly hinted at!
+        // GenericValueContainer<int>
+        $a = new GenericValueContainer(5);
+        $this->assertTrue($a->extract() === 5);
+
+        // The main thing you're looking at here is that the hint returns "@var int $c".
+        $b = new TraitConsumer(3);
+        /** @phpstan-assert string $c */
+        $c = $b->extract();
+        $dump = \PHPStan\dumpType($c);
+        $this->assertTrue($c === 3);
+    }
+}
+
+ /**
+  * Can you get PHPStan to hint at the contained value during extraction?
+  *
+  * @template IdentityValue The identity contained inside the functor.
+  */
+class GenericValueContainer
+{
+    /**
+     * @var IdentityValue
+     */
+    public mixed $value;
+
+    /**
+     * @param IdentityValue $value
+     */
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return IdentityValue
+     */
+    public function extract()
+    {
+        return $this->value;
+    }
+}
+
+/**
+ * @template IdentityValue The identity contained inside the functor.
+ */
+trait GenericPointedTrait
+{
+    /**
+     * @var IdentityValue
+     */
+    protected mixed $value;
+
+    /**
+     * Ensure everything on start.
+     *
+     * @param IdentityValue $value
+     */
+    public function __construct(mixed $value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return IdentityValue
+     */
+    public function extract()
+    {
+        return $this->value;
+    }
+}
+
+/**
+ * How to consume a trait that contains PHPStan generics.
+ *
+ * @link https://github.com/phpstan/phpstan/issues/9630 Begin the rabbit hole.
+ * @link https://github.com/phpstan/phpstan/issues/11160 Follow the rabbit hole.
+ * @link https://phpstan.org/r/af42e09b-2d4a-4bac-b837-e6c3e34b8ab9 Rabbit's dead: types without generics.
+ * @link https://phpstan.org/r/077a11ae-e527-4dc1-8f91-975267d73643 Rabbit's deader: Types WITH generics.
+ * @link https://phpstan.org/writing-php-code/phpdoc-types#offset-access
+ *   The only reference for @use and traits I could find.
+ *
+ * @template IdentityValue
+ */
+class TraitConsumer
+{
+    /** @use GenericPointedTrait<IdentityValue> */
+    use GenericPointedTrait;
+
+    public function foo(): mixed
+    {
+        return $this->extract();
+    }
+}
