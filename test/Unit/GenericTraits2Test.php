613282fb3f36582c1a63af97eb66324b5161055a
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2024-07-10 05:17:21 -0400

    Generic loss is not related to inheritance.

--- test/Unit/GenericTraits2Test.php
+++ test/Unit/GenericTraits2Test.php
@@ -38,25 +38,61 @@ class GenericTraits2Test extends TestCase
 }
 
 /**
- * Enforces constructor signature through both PHP and PHPStan.
- *
- * @see https://phpstan.org/blog/solving-phpstan-error-unsafe-usage-of-new-static
- *
- * @template ConstructorValue
- */
-interface ConsistentConstructorOf
-{
-    /**
-     * @param ConstructorValue $value
-     */
-    public function __construct($value);
-}
-
-
-/**
  * @template IdentityValue The identity contained inside the functor.
  */
 trait GenericPointedTrait2
+{
+    /**
+     * @var IdentityValue
+     */
+    protected mixed $value;
+
+    /**
+     * Ensure everything on start.
+     *
+     * @param IdentityValue $value
+     */
+    public function __construct(mixed $value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return IdentityValue
+     */
+    public function extract()
+    {
+        return $this->value;
+    }
+}
+
+/**
+ * @template IdentityValue
+ *
+ * When implementing the interface:
+ * dumpType() gives the correct hints for TraitConsumerOf<int> or TraitConsumerOf<string>
+ * However, PHPStan on the IDE reports TraitConsumerOf<mixed>  :'(
+ *
+ * Removing the ConsistentConstructorOf interface restores the correct generic functionality
+ * to the hints, but then I get hit with "Unsafe usage of new static()"
+ *
+ * SOLVED: Using the other alternatives mentioned by PHPStan docs does solve the generic
+ * loss, with plenty of approaches supported for different scenarios.
+ *
+ * I'm sticking with the `consistent-constructor` tag because it supports the more open ended inheritance case.
+ * But for more locked down inheritance models there's also solutions for that (such as final constructors).
+ *
+ * @link @see https://phpstan.org/blog/solving-phpstan-error-unsafe-usage-of-new-static
+ * @link https://github.com/phpstan/phpstan/discussions/11302 My final response to this issue.
+ * @link https://phpstan.org/r/7a4a4edb-a2f1-467a-bcef-4037ce45f6c9 The cnstructor tag supports child overloading!
+ * @link https://drupal.slack.com/archives/C033S2JUMLJ/p1720436243502369
+ *   Drupal.org chit-chat about the interface edge case losing generic typing with @AndyF, @AlexanderAllen
+ * @link https://www.drupal.org/docs/develop/development-tools/phpstan/handling-unsafe-usage-of-new-static
+ *   Updated dox on DO for this edge case.
+ *
+ * @phpstan-consistent-constructor
+ */
+class TraitConsumerOf
 {
     /**
      * @var IdentityValue
@@ -89,37 +125,6 @@ trait GenericPointedTrait2
     {
         return new static($value);
     }
-}
-
-/**
- * @template IdentityValue
- *
- * When implementing the interface:
- * dumpType() gives the correct hints for TraitConsumerOf<int> or TraitConsumerOf<string>
- * However, PHPStan on the IDE reports TraitConsumerOf<mixed>  :'(
- *
- * Removing the ConsistentConstructorOf interface restores the correct generic functionality
- * to the hints, but then I get hit with "Unsafe usage of new static()"
- *
- * SOLVED: Using the other alternatives mentioned by PHPStan docs does solve the generic
- * loss, with plenty of approaches supported for different scenarios.
- *
- * I'm sticking with the `consistent-constructor` tag because it supports the more open ended inheritance case.
- * But for more locked down inheritance models there's also solutions for that (such as final constructors).
- *
- * @link https://github.com/phpstan/phpstan/discussions/11302 My final response to this issue.
- * @link https://phpstan.org/r/7a4a4edb-a2f1-467a-bcef-4037ce45f6c9 The cnstructor tag supports child overloading!
- * @link https://drupal.slack.com/archives/C033S2JUMLJ/p1720436243502369
- *   Drupal.org chit-chat about the interface edge case losing generic typing with @AndyF, @AlexanderAllen
- * @link https://www.drupal.org/docs/develop/development-tools/phpstan/handling-unsafe-usage-of-new-static
- *   Updated dox on DO for this edge case.
- *
- * @phpstan-consistent-constructor
- */
-class TraitConsumerOf
-{
-    /** @use GenericPointedTrait2<IdentityValue> */
-    use GenericPointedTrait2;
 }
 
 
